414631e796015993d444499fa253a5483471b2d4
Tim Weri <ng.duc.tahn@gmail.com>
2019-02-21 02:10:46 -0500

    Link mongodb container with app

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var express = require('express');
-
+const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://db-mongo:27017/uwcourseapi', {useNewUrlParser: true});
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
