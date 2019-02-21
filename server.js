36f1e177f46032dbba3f573bbfafb860efbbe0a1
Tim Weri <ng.duc.tahn@gmail.com>
2019-02-21 04:25:28 -0500

    Launch app after database connect

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const PORT = 5000;
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://db-mongo:27017/uwcourseapi', {useNewUrlParser: true});
+const db = mongoose.connection;
 
-const PORT = 5000;
+db.on('error', console.error.bind(console, 'Connection error: unable to connect to uwcourseapi database'));
+db.once('open', () => {
+   console.log('Successfully connected to database uwcourseapi');
 
-app.listen(PORT, () => {
-   console.log(`Server running on port ${PORT}`);
+   app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+   });
 });
