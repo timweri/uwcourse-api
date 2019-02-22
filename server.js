c50ecd0b3c3c2ae6f70e489da30c863765ddb453
Tim Weri <ng.duc.tahn@gmail.com>
2019-02-22 04:17:12 -0500

    Change spacing

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Connection error: unable to connect to uwcourseapi database'));
 db.once('open', () => {
-   console.log('Successfully connected to database uwcourseapi');
+    console.log('Successfully connected to database uwcourseapi');
 
-   app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-   });
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
