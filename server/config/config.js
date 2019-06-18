49939a7ecc8dbb937774584e1fcd699ec2bead3a
Tim Weri <ng.duc.tahn@gmail.com>
2019-06-17 22:30:07 -0400

    Update general server config Added Winston logger, app-root-path, node-gyp and require-dir. Use a config file instead of dotenv.
    Signed-off-by: Tim Weri <ng.duc.tahn@gmail.com>

---
+++ server/config/config.js
@@ -0,0 +1,35 @@
+const env = process.env.NODE_ENV;
+
+const endpoint = '/api/v1';
+
+const dev = {
+    app: {
+        port: parseInt(process.env.DEV_APP_PORT) || 5000,
+        endpoint: process.env.ENDPOINT || endpoint,
+    },
+    db: {
+        host: process.env.DEV_DB_HOST || 'mongodb://mongodb',
+        port: parseInt(process.env.DEV_DB_PORT) || 27017,
+        name: process.env.DEV_DB_NAME || 'uwcourseapi',
+    },
+    secret: 'I<3TomYam',
+};
+
+const test = {
+    app: {
+        port: parseInt(process.env.TEST_APP_PORT) || 5000,
+        endpoint: process.env.ENDPOINT || endpoint,
+    },
+    db: {
+        host: process.env.TEST_DB_HOST || 'mongodb://mongodb',
+        port: parseInt(process.env.TEST_DB_PORT) || 27017,
+        name: process.env.TEST_DB_NAME || 'test',
+    },
+};
+
+const config = {
+    dev,
+    test,
+};
+
+module.exports = config[env];
--- server/server.js
+++ server/server.js
@@ -1,29 +1,32 @@
-require('dotenv').config();
+const logger = require('./config/winston')(__filename.slice(__dirname.length + 1, -3));
 
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const router = express.Router();
-const stage = require('./config');
 const routes = require('./routes/index.js');
 
+const config = require('./config/config');
+
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true,
 }));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://mongodb:27017/uwcourseapi', {useNewUrlParser: true});
+mongoose.connect(`${config.db.host}:${config.db.port}/${config.db.name}`, {useNewUrlParser: true});
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'Connection error: unable to connect to uwcourseapi database'));
+db.on('error', logger.error.bind(logger, 'Connection error: unable to connect to uwcourseapi database'));
 
 db.once('open', () => {
-    console.log('Successfully connected to database uwcourseapi');
+    logger.verbose('Successfully connected to database uwcourseapi');
 
-    app.use('/api/v1', routes(router));
+    app.use(config.app.endpoint, routes(router));
 
-    app.listen(`${stage.development.port}`, () => {
-        console.log(`Server now listening at localhost:${stage.development.port}`);
+    app.listen(config.app.port, () => {
+        logger.verbose(`Server now listening at localhost:${config.app.port}`);
+        console.log(`Server now listening at localhost:${config.app.port}`);
     });
 });
